ecf60336ee4dfa367e4e7ea2c8c53f9155b92fca
Premraj <premrajzambre@gmail.com>
2021-02-26 18:27:58 +0530

    Api Changed to weatherapi.com

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, {useState} from 'react';
 
-const weatherApi = {
-  key : "8eadc328b47149521321cf2bb779cf36",
-  base : "https://api.openweathermap.org/data/2.5/"
+const api = {
+  key : "2dd3a21c523243a8b9082823212302",
+  base : "https://api.weatherapi.com/v1/current.json"
 }
 
-const timeApi = {
-  key : "YNV6KRHE7W2R",
-  base : "http://api.timezonedb.com/v2.1/"
-}
-
-let lat = "";
-let lon = "";
 let format = '';
-let tBack = ''
+let tBack = '';
 
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
-  const [time, setTime] = useState({});
 
   const search = evt => {
     if(evt.key === "Enter") {
-      fetch(`${weatherApi.base}weather?q=${query}&units=metrics&APPID=${weatherApi.key}`)
+      fetch(`${api.base}?key=${api.key}&q=${query}`)
       .then(res => res.json())
       .then(result => {
         getTime(result);
@@ -33,45 +25,31 @@ function App() {
     }
   }
 
-  function getTime(d) {
-    lat = d.coord.lat;
-    lon = d.coord.lon;
-
-    fetch(`${timeApi.base}get-time-zone?key=${timeApi.key}&format=json&by=position&lat=${d.coord.lat}&lng=${d.coord.lon}`)
-    .then(res => res.json())
-    .then(data => {
-      putTime(data);
-      setTime(data);
-      setQuery('');
-      console.log(data);
-    })
-  }
-
-  function putTime(t){
-    format = t.formatted;
+  function getTime(t){
+    format = t.location.localtime;
+    console.log('Format : ',format);
     format = format.split(" ");
     tBack = format[1].split(":");
-    // console.log(tBack);
+    console.log(tBack);
   }
 
-  const dateBuilder = (d) => {
+  const dateBuilder = () => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day}, ${month} ${date} ${year}`
+    let dt = format[0].split("-");
+    
+    let date = dt[2];
+    let month = months[dt[1]-1];
+    let year = dt[0];
+   
+    return `${month} ${date}, ${year}`
   }
 
   function appSelect(season){
     if(season == 'Clear' || season == 'Snow')
     {
-      if((weather.main.temp-273.15) < 17)
+      if((weather.current.temp_c) < 17)
       {
-        if(tBack[0]>6 && tBack[0]<19)
+        if(weather.current.is_day == 1)
         {
           return 'app coldDay';
         }
@@ -112,10 +90,13 @@ function App() {
         return 'app cloudNight';
       }
     }
+    else{
+      return 'app';
+    }
   }
 
   return (
-    <div className={(typeof weather.main != "undefined") ? appSelect(weather.weather[0].main) : 'app'}>
+    <div className={(typeof weather.current != "undefined") ? appSelect(weather.current.condition.text) : 'app'}>
       <main>
         <div className="search-box">
           <input
@@ -127,23 +108,26 @@ function App() {
             onKeyPress={search}
           />
         </div>
-        {(typeof weather.main != "undefined") ? (
+        {(typeof weather.current != "undefined") ? (
         <div>
           <div className="location-box">
-            <div className="location">{weather.name}, {time.countryName}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
+            <div className="location">{weather.location.name}, {weather.location.region}, {weather.location.country}</div>
+            <div className="date">{dateBuilder()}</div>
           </div>
           <div className="weather-box">
-            <div className="temp">{Math.round(weather.main.temp-273.15)}°c</div>
-            <div className="weather">{weather.weather[0].main}</div>
+            <div className="temp">{weather.current.temp_c}°c</div>
+            <div className="weather">{weather.current.condition.text}</div>
           </div>
           <div className="time-box">
             <div className="time">{format[1]}</div>
-            <div className="zone">{time.zoneName}</div>
+            <div className="zone">{weather.location.tz_id}</div>
           </div>
         </div>
       ) : ('')}
       </main>
+      <footer className="backLink">
+      Powered by <a href="https://www.weatherapi.com/" title="Free Weather API">WeatherAPI.com</a>
+      </footer>
     </div>
   );
 }
